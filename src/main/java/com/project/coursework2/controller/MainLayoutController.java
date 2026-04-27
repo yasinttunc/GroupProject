b022e939396628af3135036da0a1b33137a444f7
yasinttunc <yasin.ttunc@gmail.com>
2026-04-27 10:59:42 +0100

    Revert "Merge pull request #8 from yasinttunc/jazz-account-dashboard"
    This reverts commit 76cd7a2138111d366d93f45f9a903762f29d4cf3, reversing
    changes made to 21b9d83d84bd121b4dc004a2fce4a62695ea1379.

--- src/main/java/com/project/coursework2/controller/MainLayoutController.java
+++ src/main/java/com/project/coursework2/controller/MainLayoutController.java
@@ -6,22 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 import java.io.IOException;
 
-/**
- * Controller for the main application layout.
- * Initialises the sidebar and loads the home page on startup.
- * @author Jasmine Eagles
- * @version 1.0
- */
 public class MainLayoutController {
 
-    /** The central content area where pages are loaded. */
     @FXML
     private StackPane contentArea;
 
-    /**
-     * Initialises the layout by connecting the sidebar to the content area
-     * and loading the home page.
-     */
     @FXML
     public void initialize() {
         SidebarController.mainContentArea = contentArea;
--- src/main/java/com/project/coursework2/controller/ResourcesController.java
+++ src/main/java/com/project/coursework2/controller/ResourcesController.java
@@ -2,17 +2,8 @@ package com.project.coursework2.controller;
 
 import javafx.fxml.FXML;
 
-/**
- * Controller for the Resources page.
- * Displays available rooms and equipment for booking.
- * @author Jasmine Eagles
- * @version 1.0
- */
 public class ResourcesController {
 
-    /**
-     * Initialises the Resources page.
-     */
     @FXML
     public void initialize() {
         System.out.println("Resources page loaded");
